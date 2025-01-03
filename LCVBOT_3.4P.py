cdc9c3bd8d033e2164bd3cc01afd2a0ba497f438
LCVTools <lcvtools@gmail.com>
2025-01-03 15:32:33 +0700

    Update LCVBOT_3.4P.py
    penambahan
     from langchain_huggingface import HuggingFaceEmbeddings

--- LCVBOT_3.4P.py
+++ LCVBOT_3.4P.py
@@ -4,6 +4,7 @@ from langchain_groq import ChatGroq
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
